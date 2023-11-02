c2822ec6c9ef5b9d9532e5ed9add39adae6ccf1f
atin-sahu <jayshiv7171@gmail.com>
2023-11-02 23:59:48 +0530

    done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import { COLORS } from '../constants'
+import { COLORS, inputTheme } from '../constants'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AntDesign } from '@expo/vector-icons';
+import { Button, TextInput } from 'react-native-paper';
+import { setIsEdit, updateUserPersionalForm } from '../store/userSlice';
 
 const Profile = () => {
 
+  const dispatch = useDispatch();
+
   const { institutes } = useSelector((state) => state.education)
+  const { isEdit, userPersionalForm } = useSelector((state) => state.user)
+  console.log("isEdit--", isEdit)
+
+  const handleUpdate = () => {
+    dispatch(setIsEdit(false));
+  }
 
   return (
     <View style={styles.main}>
@@ -19,62 +29,118 @@ const Profile = () => {
           <Image style={styles.profileIMage} source={require('../../assets/profile.jpg')}></Image>
         </View>
 
-        <View style={styles.personalData}>
-          <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'baseline'}}>
-            <Text style={styles.titleText}>Personal Details :</Text>
-            <AntDesign name="edit" size={20} color={COLORS.error}  />
-          </View>
-          <Text style={styles.name}>Atin Sahu</Text>
-          <Text style={styles.designation}>Frontend Developer </Text>
-          <Text style={styles.dateOfBirth}>06/03/2002</Text>
-          <Text style={styles.dateOfBirth}>Male</Text>
-          <Text style={styles.dateOfBirth}>Unmarried</Text>
-          <Text style={styles.dateOfBirth}>Narayan Kheda, Bani, Banthra, Sarojani Nagar, Lucknow, U.P. India </Text>
+        {!isEdit
+          ? (
+            <View style={styles.personalData}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline' }}>
+                <Text style={styles.titleText}>Personal Details :</Text>
+                <AntDesign name="edit" size={20} color={COLORS.error} onPress={() => dispatch(setIsEdit(true))} />
+              </View>
+              <Text style={styles.name}>Atin Sahu</Text>
+              <Text style={styles.designation}>Frontend Developer </Text>
+              <Text style={styles.dateOfBirth}>06/03/2002</Text>
+              <Text style={styles.dateOfBirth}>Male</Text>
+              <Text style={styles.dateOfBirth}>Unmarried</Text>
+              <Text style={styles.dateOfBirth}>Narayan Kheda, Bani, Banthra, Sarojani Nagar, Lucknow, U.P. India </Text>
 
-          {institutes?.length
-            ? <Text style={styles.titleText}>Education Details :</Text>
-            : null
-          }
+              {institutes?.length
+                ? <Text style={styles.titleText}>Education Details :</Text>
+                : null
+              }
 
-          {institutes?.length
-            ? institutes?.map((ins, ind) => {
-              return (
-                <View key={ind} style={{ marginTop: 6 }}>
-                  <Text style={styles.educationName}>{ins?.institute}</Text>
-                  <Text style={styles.educationStyle}>{ins?.course}</Text>
-                  <Text style={styles.educationStyle}>{ins?.startDate ? ins?.startDate : '01-06-2022'} - {ins?.endDate ? ins?.endDate : '01-06-2023'}</Text>
-                </View>
-              )
-            })
-            : null
-          }
+              {institutes?.length
+                ? institutes?.map((ins, ind) => {
+                  return (
+                    <View key={ind} style={{ marginTop: 6 }}>
+                      <Text style={styles.educationName}>{ins?.institute}</Text>
+                      <Text style={styles.educationStyle}>{ins?.course}</Text>
+                      <Text style={styles.educationStyle}>{ins?.startDate ? ins?.startDate : '01-06-2022'} - {ins?.endDate ? ins?.endDate : '01-06-2023'}</Text>
+                    </View>
+                  )
+                })
+                : null
+              }
 
-          <Text style={styles.titleText}>Contact Details :</Text>
+              <Text style={styles.titleText}>Contact Details :</Text>
 
-          <View style={styles.emailView}>
-            <MaterialCommunityIcons name="phone" size={22} color={COLORS.secondary} style={{ marginTop: 6 }} />
-            <Text style={styles.dateOfBirth}>+91 63919 771771</Text>
-          </View>
+              <View style={styles.emailView}>
+                <MaterialCommunityIcons name="phone" size={22} color={COLORS.secondary} style={{ marginTop: 6 }} />
+                <Text style={styles.dateOfBirth}>+91 63919 771771</Text>
+              </View>
 
-          <View style={styles.emailView}>
-            <MaterialCommunityIcons name="gmail" size={22} color={COLORS.secondary} style={{ marginTop: 6 }} />
-            <Text style={styles.dateOfBirth}>jayshiv7171@gmail.com</Text>
-          </View>
+              <View style={styles.emailView}>
+                <MaterialCommunityIcons name="gmail" size={22} color={COLORS.secondary} style={{ marginTop: 6 }} />
+                <Text style={styles.dateOfBirth}>jayshiv7171@gmail.com</Text>
+              </View>
 
-          <View style={styles.emailView}>
-            <MaterialCommunityIcons name="gmail" size={22} color={COLORS.secondary} style={{ marginTop: 6 }} />
-            <Text style={styles.dateOfBirth}>atinsahu71@gmail.com</Text>
-          </View>
+              <View style={styles.emailView}>
+                <MaterialCommunityIcons name="gmail" size={22} color={COLORS.secondary} style={{ marginTop: 6 }} />
+                <Text style={styles.dateOfBirth}>atinsahu71@gmail.com</Text>
+              </View>
 
-          <Text style={styles.titleText}>Social Accounts :</Text>
-          <View style={{flexDirection:'row', gap:10, justifyContent:'center', marginTop:10 }}>
-            <MaterialCommunityIcons name="twitter" size={50} color={COLORS.secondary} />
-            <MaterialCommunityIcons name="linkedin" size={50} color={COLORS.secondary} />
-            <MaterialCommunityIcons name="facebook" size={50} color={COLORS.secondary} />
-            <MaterialCommunityIcons name="instagram" size={50} color={COLORS.secondary} />
-          </View>
-          <Text style={{ marginBottom: 20 }}></Text>
-        </View>
+              <Text style={styles.titleText}>Social Accounts :</Text>
+              <View style={{ flexDirection: 'row', gap: 10, justifyContent: 'center', marginTop: 10 }}>
+                <MaterialCommunityIcons name="twitter" size={50} color={COLORS.secondary} />
+                <MaterialCommunityIcons name="linkedin" size={50} color={COLORS.secondary} />
+                <MaterialCommunityIcons name="facebook" size={50} color={COLORS.secondary} />
+                <MaterialCommunityIcons name="instagram" size={50} color={COLORS.secondary} />
+              </View>
+              <Text style={{ marginBottom: 20 }}></Text>
+            </View>
+          )
+          : (
+
+            <View>
+              <View style={styles.addProfileView}>
+
+                <TextInput
+                  mode='flat'
+                  label={'Name'}
+                  placeholder='Enter Name'
+                  value={userPersionalForm.name}
+                  onChangeText={(value) => dispatch(updateUserPersionalForm({ ...userPersionalForm, name: value }))}
+                  style={styles.inputStyle}
+                  theme={inputTheme}
+                />
+
+                <TextInput
+                  mode='flat'
+                  label={'designation'}
+                  placeholder='Enter work designation '
+                  value={userPersionalForm?.designation}
+                  onChangeText={(value) => dispatch(updateUserPersionalForm({ ...userPersionalForm, designation: value }))}
+                  style={styles.inputStyle}
+                  theme={inputTheme}
+                />
+
+                <TextInput
+                  mode='flat'
+                  label={'Address'}
+                  placeholder='Enter address'
+                  value={userPersionalForm?.address}
+                  onChangeText={(value) => dispatch(updateUserPersionalForm({ ...userPersionalForm, address: value }))}
+                  multiline={true}
+                  numberOfLines={5}
+                  style={styles.inputStyle}
+                  theme={inputTheme}
+                />
+
+              </View>
+
+              <View style={styles.addBtnView}>
+                <Button
+                  mode="contained"
+                  onPress={handleUpdate}
+                  style={{ backgroundColor: COLORS.primaryLight }}
+                >
+                  <Text style={{ color: COLORS.secondary, fontSize: 18, fontWeight: '800' }}>{'Update & Save'}</Text>
+                </Button>
+              </View>
+            </View>
+          )
+        }
+
+
 
       </ScrollView>
     </View>
@@ -146,4 +212,21 @@ const styles = StyleSheet.create({
     color: COLORS.secondary,
     fontStyle: "italic",
   },
+  addProfileView: {
+    marginVertical: 10,
+    backgroundColor: COLORS.primaryLight,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 15,
+    gap: 10
+  },
+  inputStyle: {
+    backgroundColor: 'white',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  addBtnView: {
+    marginTop: 10,
+    marginBottom: 20,
+  }
 })
--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,19 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initUserPersionalForm = {
+    name:'',
+    designation:'',
+    address:''
+}
+
 const userSlice = createSlice({
     name: "userSlice",
     initialState: {
         loading: false,
         user: {},
+        isEdit:false,
+        userPersionalForm:initUserPersionalForm,
     },
     reducers: {
         setUser:((state, action) => {
             state.user = action.payload;
-        })
+        }),
+        setIsEdit:(state,action) => {
+            state.isEdit = action.payload
+        },
+        updateUserPersionalForm:(state,action)=>{
+            state.userPersionalForm = action.payload
+        }
     },
 });
 
 export const {
-    setUser
+    setUser,
+    setIsEdit,
+    updateUserPersionalForm
 } = userSlice.actions;
 export default userSlice.reducer;
